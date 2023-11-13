e021c89cfd230d38213d811eaf0c2b48777433d8
1122viluoz <vicente.luongo@sansano.usm.cl>
2023-11-13 12:16:39 -0300

    ventana añadir reemplazo

--- src/Admin.js
+++ src/Admin.js
@@ -5,15 +5,18 @@ import InfoBloque from "./components/InfoBloque/InfoBloque";
 import Modal from "./components/Modal/Modal";
 import Turno from "./components/Turno/Turno";
 import './styles.css';
+import ModalReem from "./components/Modal/ModalReemp.css";
 import axios from 'axios';
 
 // const Arreglo = [{"nombre":"Christian Barrios", "estado": "En turno", "tipoTutor" : "Tutor/a de Mat/Fis"}, {"nombre":"Sofia Rios", "estado": "Ausente", "tipoTutor" : "Tutor/a de Mat/Fis"}];
 
 
 function Admin() {
+  const [editing, setEditing] = useState(false);
   const [mostrarModal, setMostrarModal] = useState(false);
-  const [data, setData] = useState([]);
-  
+  const [ModalR, setModal] = useState(false);
+  const [textoEditable, setTextoEditable] = useState("Ingresa tu texto aquí...");
+
   useEffect(() => {
     // Realiza una solicitud GET a la API
     axios.get('http://localhost:9000/api/tutores')
@@ -62,14 +65,22 @@ function Admin() {
 
   
 
+  const changeModal = () => {
+    setModal(!ModalR);
+  }
+
   const abrirModal = () => {
     setMostrarModal(true);
   };
 
   const cerrarModal = () => {
-    setMostrarModal(false);
+    setModal(false);
   };
-
+  const handleContentEditableClick = (e) => {
+    // Detener la propagación del evento para evitar cerrar el modal
+    setTextoEditable("");
+    e.stopPropagation();
+  };
 
 
   return(
@@ -95,12 +106,30 @@ function Admin() {
         <div className={s.block}>
           <InfoBloque name="Axel Kaempffer" bloque="3-4"/>
           <button className={s.button} onClick={abrirModal}>Ingresar turno</button>
-          <button className={s.button} onClick={actualizarDatos}>Ingresar reemplazo</button>
+          <button className={s.button} onClick={changeModal}>Ingresar reemplazo</button>
         </div>
       </div>
       <div>
     </div>
     <Modal show={mostrarModal} cerrarModal={cerrarModal} className={s.modal}/>
+    {ModalR && (
+      <div className={`overlay ${ModalR ? 'show' : 'hide'}`} onClick={changeModal}>
+      <div className={`modal ${ModalR ? 'show' : 'hide'}`}>
+        <div className='modal-content'>
+          <h2>Información de reemplazo</h2>
+          <p>ROL de quién se reemplaza</p>
+          <div
+            className={`editable-div ${editing ? 'editing' : ''}`}
+            contentEditable="true"
+            onClick={handleContentEditableClick}
+        >
+            {textoEditable}
+        </div>
+          <button className='close-modal' onClick={changeModal}>X</button>
+        </div>
+      </div>
+    </div>
+    )}
     </article>
     </>
   );
