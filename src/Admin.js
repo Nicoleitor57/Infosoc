dd65b5399660ec857fa772871a5850b942d62287
Francisca Figueroa <fcafigueroal@gmail.com>
2023-11-16 16:06:31 -0300

    feat: cambiar bloque

--- src/Admin.js
+++ src/Admin.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import s from './Admin.module.css';
 import './ModalReemp.css';
 import Header from "./components/Header/Header";
@@ -6,7 +6,12 @@ import InfoBloque from "./components/InfoBloque/InfoBloque";
 import Turno from "./components/Turno/Turno";
 import './styles.css';
 
-const Arreglo = [{"nombre":"Christian Barrios", "estado": "En turno", "tipoTutor" : "Tutor/a de Mat/Fis"}, {"nombre":"Sofia Rios", "estado": "Ausente", "tipoTutor" : "Tutor/a de Mat/Fis"}];
+const Arreglo = [
+  {"nombre":"Christian Barrios", "estado": "En turno", "tipoTutor" : "Tutor/a de Mat/Fis", "bloque": "3-4"}, 
+  {"nombre":"Sofia Rios", "estado": "Ausente", "tipoTutor" : "Tutor/a de Mat/Fis", "bloque": "3-4"},
+  {"nombre":"Vicente Luongo", "estado": "Ausente", "tipoTutor" : "Tutor/a de Mat/Fis", "bloque": "5-6"},
+  {"nombre":"Gabriel Venegas", "estado": "Ausente", "tipoTutor" : "Tutor/a de Mat/Fis", "bloque": "7-8"},
+];
 
 
 function Admin() {
@@ -19,6 +24,10 @@ function Admin() {
   const [accessError, setAccessError] = useState(false);
   const [accessSuccess, setAccessSuccess] = useState(false);
   const [ inputError, setInputError ] = useState(false);
+  const bloques = ['1-2','3-4','5-6','7-8','9-10','11-12'];
+
+  const [bloqueActualIndex, setBloqueActualIndex] = useState(0);
+
 
   const [ModalR, setModal] = useState(false);
   const [textoEditable, setTextoEditable] = useState("Ingresa tu texto aquí...");
@@ -41,27 +50,20 @@ function Admin() {
 
   //Cierra la modal de ingresar turno
   const cerrarModal = () => {
+    setInputError(false);
     setModal(false);
     setMostrarModal(false);
     setAccessError(false);
     setAccessSuccess(false);
-    
+    setMostrarForm(false);
+    setModal(false);
+    setId('');
+    console.log(inputError);
   };
-
-  const handleContentEditableClick = (e) => {
-    // Detener la propagación del evento para evitar cerrar el modal
-    setTextoEditable("");
-    e.stopPropagation();
-  };
-
 
   //Comprueba que el ID exista en la base de datos de los tutores.
   const comprobarId = () => {
     console.log('ID ingresado:', id);
-
-    if(id === ''){
-      setInputError(true);
-    }
 
     const comprobacion = true;
 
@@ -73,10 +75,26 @@ function Admin() {
     } 
   };
 
-  useEffect(()=>{
-    setInputError(false);
-    console.log("--------lol-----");
-  },[inputError]);
+  const inputErrorFuntion = () => {
+
+    if(rol === ''){
+      setInputError(true);
+    }
+    
+    if(id === ''){
+      setInputError(true);
+    }
+
+    setTimeout(() => {
+      setInputError(false);
+    }, 500);
+
+  };
+  
+  const cambiarBloque = (indice) => {
+    const nuevoIndice = (bloqueActualIndex + indice + bloques.length) % bloques.length;
+    setBloqueActualIndex(nuevoIndice);
+  };
 
   //Comprueba que el rol exista en la base de datos de los tutores.
   const comprobarRol = () => {
@@ -84,10 +102,6 @@ function Admin() {
     console.log('Rol ingresado:', rol);
 
     const comprobacion = true;
-
-    if(rol === ''){
-      setInputError(true);
-    }
 
     if (comprobacion && rol !== ''){
       setAccessSuccess(true);
@@ -114,11 +128,23 @@ function Admin() {
     <>
     <Header/>
     <article className={s.wrapper}>
-      <h1>Turnos actuales</h1>
+      <div className={s.title}>
+        <h1>Turnos actuales</h1>
+        <div className={s.bloque}>
+          <button className={s.icon}>
+              <img src="images/arrow-left.svg" alt="Flecha hacia la izquierda" onClick={() => cambiarBloque(-1)}/>
+          </button>
+          <span className={s.bloqueName}>{bloques[bloqueActualIndex]}</span>
+          <button className={s.icon}>
+              <img src="images/arrow-right.svg" alt="Flecha hacia la izquierda" onClick={() => cambiarBloque(1)}/>
+          </button>
+        </div>
+      </div>
       <div className={s.box}>
         <div className={s.turnos}>
-        {Arreglo.map((item) => (         
+        {Arreglo.filter(item => item.bloque === bloques[bloqueActualIndex]).map((item) => (         
           <Turno
+            key={item.nombre}
             name={item.nombre}
             tipoTutor={item.tipoTutor}
             state={
@@ -153,11 +179,10 @@ function Admin() {
             <text className={s.subtitle}>Lee el código QR del tutor/a</text>
             <div className={s.codigoQR}>
               <img src="images/codigoQR.png" alt="Imagen de un código QR" className={s.image}/>
-              <input type="text" name="id" value={id} onChange={recuperarId} className={`${inputError ? s.inputError : s.input}`}
-/>
+              <input type="text" name="id" value={id} onChange={recuperarId} className={`${inputError ? s.inputError : s.input}`}/>
             </div>
             <div className={s.buttons}>
-              <button className={s.button} onClick={comprobarId}>Ingresar</button>
+              <button className={s.button} onClick={() => { comprobarId(); inputErrorFuntion(); }}>Ingresar</button>
               <button onClick={changeForm} className={s.button2}>Ingresar de forma manual</button>
             </div>
               {accessSuccess && (
@@ -183,7 +208,7 @@ function Admin() {
             <section className={s.form}>
               <div className={s.title}>
           <h2>Ingresar turno manualmente</h2>
-          <button onClick={changeForm} className={s.icon}>
+          <button onClick={cerrarModal} className={s.icon}>
             <img src="images/close.svg" alt="Icono cerrar"/>
           </button>
         </div>
@@ -195,7 +220,7 @@ function Admin() {
           <div className={s.buttons}>
             <button 
             className={s.button} 
-            onClick={comprobarRol}>
+            onClick={() => { comprobarRol(); inputErrorFuntion(); }}>
               Ingresar
             </button>
           </div>
@@ -217,19 +242,40 @@ function Admin() {
                   
     )}
     {ModalR && (
-      <div className={`overlay ${ModalR ? 'show' : 'hide'}`} onClick={changeModal}>
+      <div className={`overlay ${ModalR ? 'show' : 'hide'}`}>
       <div className={`modal ${ModalR ? 'show' : 'hide'}`}>
-        <div className='modal-content'>
-          <h2>Información de reemplazo</h2>
-          <p>ROL de quién se reemplaza</p>
-          <div
-            className={`editable-div ${editing ? 'editing' : ''}`}
-            contentEditable="true"
-            onClick={handleContentEditableClick}
-        >
-            {textoEditable}
+      <div className='modal-content'>
+            <section className={s.form}>
+              <div className={s.title}>
+          <h2>Ingresar reemplazo</h2>
+          <button onClick={cerrarModal} className={s.icon}>
+            <img src="images/close.svg" alt="Icono cerrar"/>
+          </button>
         </div>
-          <button className='close-modal' onClick={changeModal}>X</button>
+        <div className={s.box2}>
+          <div className={s.label}>
+            <label>Rol:</label>
+            <input type="text" name="rol" value={rol} onChange={recuperarRol} className={`${inputError ? s.inputError : s.input}`}/>
+          </div>
+          <div className={s.buttons}>
+            <button 
+            className={s.button} 
+            onClick={() => { comprobarRol(); inputErrorFuntion(); }}>
+              Ingresar
+            </button>
+          </div>
+        </div>
+            {accessSuccess && (
+              <div className={s.success}>
+                Fue ingresado correctamente
+              </div>
+            )}
+            {accessError && (
+              <div className={s.error}>
+                No existe este tutor en nuestra base de datos
+              </div>
+            )}
+      </section>
         </div>
       </div>
     </div>
